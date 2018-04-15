7a9bac749f7e99da04a62bbe06ce52c53b7c3b2e
gaurav138 <gauravcharde13@rediffmail.com>
2018-04-16 02:15:32 +0530

    trying to update main add new file MyTry

--- app/src/main/java/com/example/w10/chatroom/MainActivity.java
+++ app/src/main/java/com/example/w10/chatroom/MainActivity.java
@@ -38,5 +38,7 @@ public class MainActivity extends AppCompatActivity {
     private void TestUpload()
     {
         int i=1+1;
+        int j=2+2;
+
     }
 }
---
+++ app/src/main/java/com/example/w10/chatroom/MyTry.java
@@ -0,0 +1,13 @@
+package com.example.w10.chatroom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyTry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_try);
+    }
+}
